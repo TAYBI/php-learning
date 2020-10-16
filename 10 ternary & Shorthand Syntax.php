0637744063784bd31a9eb2e4dbdc95da3eb437a4
TAYBI <taybib4@gmail.com>
2020-10-16 22:51:41 +0100

    ternary

---
+++ 10 ternary & Shorthand Syntax.php
@@ -0,0 +1,11 @@
+<?php
+    $loggedIn = true;
+    // if ($loggedIn) {
+    //     Echo 'logged in';
+    // }
+    // else {
+    //     Echo 'not logged in';
+    // }
+
+    echo ($loggedIn) ? 'loggeed in' : 'not logged in'
+?>
